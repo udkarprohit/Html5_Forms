bf166b2daf93b3e0711e700f8ab3e00dc9f66661
Prohit <udkarprohit@gmail.com>
2023-03-05 18:40:21 +0530

    Added: Validating User Email Address

--- validate.js
+++ validate.js
@@ -12,3 +12,17 @@ function validateFirstName() {
       firstNameInput.setCustomValidity("Invalid first name");
     }
 }
+function validateEmailAddress() {
+  const emailInput = document.getElementById("email-address");
+  const email = emailInput.value;
+  const regex = /^[a-z]{3,}[-_+.a-z0-9]*[@][0-1a-z]+[.][a-z]{2,}[.]?[,a-z]{0,3}$/;
+  const errorSpan = document.getElementById("email-address-error");
+
+  if (regex.test(email)) {
+    errorSpan.innerText = "";
+    emailInput.setCustomValidity("");
+  } else {
+    errorSpan.innerText = "Please enter a valid email";
+    emailInput.setCustomValidity("Invalid Email Address");
+  }
+}
